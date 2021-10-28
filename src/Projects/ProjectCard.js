2b031f8fa28c311a87333244b6d3d435b81280d5
Sotirios Dimitriu <sotirios.dimitriou@gmail.com>
2021-10-28 20:42:11 +0200

    started project card

--- src/Projects/ProjectCard.js
+++ src/Projects/ProjectCard.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
+import { BiLink } from "react-icons/bi";
 
 class GithubCard extends Component {
   render() {
     const { title, description, img, repoPage, repoUrl } = this.props;
     return (
       <div>
-        <a href={repoUrl} target="_blank" rel="noreferrer">
-          <h2 className="project-title">{title}</h2>
-        </a>
+        <h2 className="project-title">{title}</h2>
         <p className="card-body">{description}</p>
+        <a href={repoUrl} target="_blank" rel="noreferrer">
+          <BiLink />
+        </a>
       </div>
     );
   }
