7a92b60036d8b4ca669830cf8a050a538403f2e5
Sotirios Dimitriu <sotirios.dimitriou@gmail.com>
2021-10-28 12:49:42 +0200

    Create project

---
+++ src/Projects/ProjectCard.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class GithubCard extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default GithubCard;
---
+++ src/Projects/ProjectContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ProjectContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Prejects</h1>
+      </div>
+    );
+  }
+}
+
+export default ProjectContainer;
