e6f8aec9dd339b4a298d01284c147b26d1983337
Sotirios Dimitriu <sotirios.dimitriou@gmail.com>
2021-11-05 12:16:18 +0100

    fixed overflow on mobile

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Intro from "./Intro/Intro";
 import About from "./About/About";
 import ProjectContainer from "./Projects/ProjectContainer";
 import Contact from "./Contact/Contact";
+import Copyright from "./Footer/Copyright";
 
 function App() {
   const [color, setColor] = useState("#161625");
@@ -22,6 +23,7 @@ function App() {
       <About />
       <ProjectContainer title="Projects" />
       <Contact />
+      <Copyright />
     </div>
   );
 }
---
+++ src/Footer/Copyright.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./style/copyright.css";
+
+class Copyright extends Component {
+  render() {
+    return (
+      <div className="copyright">
+        <h3>Sotirios Dimitriou &#169;</h3>
+      </div>
+    );
+  }
+}
+
+export default Copyright;
